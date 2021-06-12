9d1e40c0674e099d71de9decd459e7659ea1c447
Emanuel <bolzicoemanuel@gmail.com>
2021-06-12 12:53:18 -0300

    onClose working

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,21 +4,21 @@ import {Link} from 'react-router-dom'
 
 export default function Card ({min, max, name, img, onClose, id}) {
     return (
-      <Link to={`/ciudad/${id}`} className={style.link}>
         <div className={style.whole}>
-          <div className={style.ciudad}>
-            {name}
-          <button className={style.button} onClick= {(id) => onClose}>X</button>
-          </div>
-          <div className={style.content}>
-            <div className={style.temp}>
-              <p className={style.min}>MIN: {min}°</p>
-              <p className={style.max}>MAX: {max}°</p>
+          <button className={style.button} onClick= {onClose}>X</button>
+          <Link to={`/ciudad/${id}`} className={style.link}>
+            <div className={style.ciudad}>
+              {name}
             </div>
-            <img className={style.img} src={"http://openweathermap.org/img/wn/"+img+"@2x.png"} alt= "current climate img"/>
-          </div>
+            <div className={style.content}>
+              <div className={style.temp}>
+                <p className={style.min}>MIN: {min}°</p>
+                <p className={style.max}>MAX: {max}°</p>
+              </div>
+              <img className={style.img} src={"http://openweathermap.org/img/wn/"+img+"@2x.png"} alt= "current climate img"/>
+            </div>
+          </Link>
         </div>  
-      </Link>
-    
+      
     );
 };
